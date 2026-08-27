aac1f847c3455410478317bb54d2759ca9742077
bhagyashree196 <bhagyashreesable97@gmail.com>
2026-08-27 23:00:24 +0530

    Javascript practice day 1

---
+++ app.js
@@ -0,0 +1,56 @@
+// console.log("hello world");
+let a = 10;
+let b = 20;
+console.log("sum is :" , a+b);
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+
+console.log("the total is : ",pencilPrice + eraserPrice ,"Rupees");
+
+//Template literals
+//use back tick
+console.log(`your pay is ${pencilPrice + eraserPrice} rupees`)
+
+//operators in JS
+//Arithmetic operator + , - , * , / , %
+let x = 10;
+let y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+//Unary Operators 
+
+p = 10;
+console.log(p++);
+console.log(++p);
+
+console.log(p--);
+console.log(--p);
+
+//Assignment Operators
+
+q = 10;
+
+console.log(q);
+
+q += 5;
+console.log(q);
+
+q -= 5;
+console.log(q);
+
+//Comparision operator < , > , <= , >= , == , !=  
+
+let age = 18;
+
+console.log(age >= 18); 
+
+//Comparision for non numbers
+
+console.log('a' < 'b');
+console.log('A' > 'a')
