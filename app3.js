9efa4dabeeec222111a2ca68c5dadd9ee79a1339
bhagyashree196 <bhagyashreesable97@gmail.com>
2026-09-05 12:48:59 +0530

    For loop practice

---
+++ app3.js
@@ -0,0 +1,29 @@
+for(let i = 1 ; i <= 5 ; i++){
+    console.log(i);
+}
+
+console.log("backwords");
+
+for(let j = 10 ; j >= 1 ; j--){
+    console.log(j);
+}
+
+console.log("Odd numbers");
+for(let odd = 1 ; odd <= 15 ; odd += 2){
+    console.log(odd);
+}
+
+console.log("backwords");
+for(let oddR = 19 ; oddR >= 1 ; oddR -= 2){
+    console.log(oddR);
+}
+
+console.log("Even numbers");
+for(let even = 2 ; even <= 10 ; even += 2){
+    console.log(even);
+}
+
+console.log("backwords");
+for(let even = 10 ; even >= 1 ; even -= 2){
+    console.log(even);
+}
